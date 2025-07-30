856e1694e8f3f143d967958d68242191ba99acb2
0xJett <jettzhang95@gmail.com>
2025-07-30 09:30:19 +0800

    feat: articles sorted by update time

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,5 @@
-import { getAllArticles } from "@/lib/mdxUtils";
-import Link from "next/link";
+import ArticleList from "@/components/article-list";
 
 export default function Home() {
-  try {
-    const list = getAllArticles();
-
-    if (list.length > 0) {
-      return (
-        <ul>
-          {list.map((item) => (
-            <li
-              key={`${item.slug} + ${item.createdAt}`}
-              className="flex flex-col gap-2"
-            >
-              <Link href={`/article/${item.slug}`}>
-                {item.frontmatter.title}
-                {item.frontmatter.description}
-                {item.frontmatter.author}
-                {new Date(item.updatedAt).toLocaleDateString()}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      );
-    } else {
-      return <div>empty</div>;
-    }
-  } catch (error) {
-    return <div>error</div>;
-  }
+  return <ArticleList />;
 }
---
+++ components/article-list/index.tsx
@@ -0,0 +1,32 @@
+import { getAllArticles } from "@/lib/mdxUtils";
+import Link from "next/link";
+
+export default function ArticleList() {
+  try {
+    const list = getAllArticles().sort((a, b) => b.updatedAt - a.updatedAt);
+
+    if (list.length > 0) {
+      return (
+        <ul>
+          {list.map((item) => (
+            <li
+              key={`${item.slug} + ${item.createdAt}`}
+              className="flex flex-col gap-2"
+            >
+              <Link href={`/article/${item.slug}`}>
+                {item.frontmatter.title}
+                {item.frontmatter.description}
+                {item.frontmatter.author}
+                {new Date(item.updatedAt).toLocaleDateString()}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      );
+    } else {
+      return <div>empty</div>;
+    }
+  } catch (error) {
+    return <div>error</div>;
+  }
+}
